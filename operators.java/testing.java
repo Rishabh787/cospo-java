d40eec40b4d68ea2ced9cb0958857e92cae841ec
ris581978@gmail.com <Rishabh Singh>
2022-11-06 20:30:20 +0530

    test operators

---
+++ operators.java/testing.java
@@ -0,0 +1,15 @@
+public class testing{
+    public static void main(String[] args) {
+        // int a = 5;
+        // int b = 10;
+        // System.out.println(a!=b);
+        // System.out.println(b!=a);
+        // System.out.println(b==a);
+        // System.out.println(a<b);
+        // System.out.println(b<a);
+        int c = 15;
+        int d = 15;
+        System.out.println(c<=d);
+        System.out.println(d<=c);
+    }
+}
