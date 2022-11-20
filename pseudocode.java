2724cab2e8065c74aed6f18161ce7dd6ce04ba1b
ris581978@gmail.com <Rishabh Singh>
2022-11-20 21:14:38 +0530

    test

--- pseudocode.java
+++ pseudocode.java
@@ -2,5 +2,7 @@ public class pseudocode{
     public static void main(String[] args) {
         System.out.print("Hello java Psyco");
         System.out.print("Namaste");
+        long name = 12003;
+        System.out.println(name);
     }
 }
