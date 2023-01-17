8cef412fc02ee72af8d9024c67d8bd80f17f589a
Rishabh <ris581978@gmail.com>
2023-01-17 18:11:38 +0530

    commited program

--- pseudocode.java
+++ pseudocode.java
@@ -1,8 +1,21 @@
 public class pseudocode{
     public static void main(String[] args) {
-        System.out.print("Hello java Psyco");
-        System.out.print("Namaste");
-        long name = 12003;
-        System.out.println(name);
+        // System.out.print("Hello java Psyco");
+        // System.out.print("Namaste");
+        // long name = 12003;
+        // System.out.println(name);
+
+        int age = 18;
+        if (age >= 18){
+            System.out.println("adult");
+        }
+        else if(age > 13 && age < 18){
+            System.out.println("teen");
+        }
+        else{
+            System.out.println("child");
+        }
+        
     }
 }
+
